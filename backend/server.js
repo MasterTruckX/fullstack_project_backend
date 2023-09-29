11c94dda57566f5dff2cc37031fe10bbd12b0b8d
unknown <jorge.orozco@cetys.edu.mx>
2023-09-28 18:11:40 -0700

    app configuration for users api use

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-const port = proces.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+const cors = require('cors')
+
+connectDB()
 
 const app = express()
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use(cors())
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/ouraMovies/users', require('./routes/usersRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
 
